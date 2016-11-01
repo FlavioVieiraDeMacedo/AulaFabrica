02ff80f111a498961423d4ed9dc910c2cf890f16
Flávio Vieira de Macedo Filho <RM74083@fiap.com.br>
2016-11-01 18:30:59 -0200

    crud

---
+++ Exemplo02/Exemplo02/Controllers/AlunoController.cs
@@ -0,0 +1,32 @@
+﻿using Exemplo02.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Exemplo02.Controllers
+{
+    public class AlunoController : Controller
+    {
+        // GET: Aluno
+        [HttpGet]
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Cadastro(Aluno a) {
+            var _context = new PortalContext();
+            _context.Aluno.Add(a);
+            _context.SaveChanges();            TempData["msg"] = "Aluno cadastrado!";            return RedirectToAction("Cadastro");
+        }
+        public ActionResult Listar()
+        {
+            return View();
+        }    
+
+    }
+
+    
+}
