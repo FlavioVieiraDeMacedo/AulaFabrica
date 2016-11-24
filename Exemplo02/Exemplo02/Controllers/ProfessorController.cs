1950c56798b1e0c7dd7f7d26d0de9427bc0fe2bc
Flavio Macedo <f.m.kmacedo@hotmail.com>
2016-11-24 19:33:03 -0200

    Crud em unica pagina Professor

---
+++ Exemplo02/Exemplo02/Controllers/ProfessorController.cs
@@ -0,0 +1,72 @@
+﻿using Exemplo02.Models;
+using Exemplo02.UnitsOfWork;
+using Exemplo02.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Exemplo02.Controllers
+{
+    public class ProfessorController : Controller
+    {
+        private UnitOfWork _unit = new UnitOfWork();
+        // GET: Professor
+        [HttpGet]
+        public ActionResult Cadastrar(string msg)
+        {
+            var viewModel = new ProfessorViewModel()
+            {
+                Mensagem = msg,
+                Professores = _unit.ProfessorRepository.Listar()
+            };
+            return View(viewModel);
+        }
+        [HttpGet]
+        public ActionResult Buscar(string nomeBusca)
+        {
+            var lista = _unit.ProfessorRepository.BuscarPor(a => a.Nome.Contains(nomeBusca));
+            var viewModel = new ProfessorViewModel
+            {
+                Professores = lista
+            };
+            return PartialView("_tabela", viewModel);
+
+        }
+        [HttpPost]
+        public ActionResult Cadastrar(ProfessorViewModel professorViewModel)
+        {
+            var professor = new Professor()
+            {
+                Nome = professorViewModel.Nome,
+                Id = professorViewModel.Id,
+                Salario = professorViewModel.Salario
+
+            };
+
+            _unit.ProfessorRepository.Cadastrar(professor);
+            _unit.Salvar();
+            return RedirectToAction("Cadastrar", new { msg = "Professor cadastrado com sucesso!" });
+        }
+        [HttpPost]
+        public ActionResult Excluir(int professorId)
+        {
+            _unit.ProfessorRepository.Remover(professorId);
+            _unit.Salvar();
+            return RedirectToAction("Cadastrar", new { msg = "Professor Deletado com sucesso!" });
+        }
+        [HttpPost]
+        public ActionResult Editar(ProfessorViewModel professorViewModel) {
+            var professor = new Professor
+            {
+                Nome=professorViewModel.Nome2,
+                Salario = professorViewModel.Salario2,
+                Id=professorViewModel.Id
+            };
+            _unit.ProfessorRepository.Alterar(professor);
+            _unit.Salvar();
+            return RedirectToAction("Cadastrar", new { msg = "Professor Alterado com sucesso!" });
+        }    
+    }
+}
---
+++ Exemplo02/Exemplo02/ViewModels/ProfessorViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Exemplo02.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Exemplo02.ViewModels
+{
+    public class ProfessorViewModel
+    {
+        public string Mensagem { get; set; }
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public Nullable<decimal> Salario { get; set; }
+        public  ICollection<Aluno> Alunos { get; set; }
+        public ICollection<Professor> Professores { get; set; }
+        public Professor professor { get; set; }
+        public string Nome2 { get; set; }
+        public Nullable<decimal> Salario2 { get; set; }
+    }
+}
